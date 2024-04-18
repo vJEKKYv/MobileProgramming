458cac413b7f2cf8f238231d43942dd362615b3c
JEKKY <wpdnjs20@gmail.com>
2024-04-18 23:23:42 +0900

    ch06 업로드

--- ch06/src/main/java/com/example/ch06/MainActivity.kt
+++ ch06/src/main/java/com/example/ch06/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.ch06
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +13,34 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        //setContentView(R.layout.activity_main)
+        //setContentView(R.layout.layout_stack_test)
+        //setContentView(R.layout.match_wrap_test)
+        //setContentView(R.layout.margein_padding_test)
+        //setContentView(R.layout.visible_test)
+        /*
+        val visibleBtn = findViewById<Button>(R.id.visibleBtn)
+        val invisibleBtn:Button = findViewById<Button>(R.id.invisibleBtn)
+        val targetView = findViewById<TextView>(R.id.TargetView)
+        visibleBtn.setOnClickListener{
+            targetView.visibility= View.VISIBLE
+        }
+        invisibleBtn.setOnClickListener{
+            targetView.visibility= View.INVISIBLE
+        }
+         */
+        setContentView(R.layout.kakao_pw_check)
+        val confirmBtn = findViewById<Button>(R.id.confirmBtn)
+        val passwd = findViewById<EditText>(R.id.psswd)
+        var check:Int =1
+        confirmBtn.setOnClickListener{
+            if (check==1){
+                passwd.visibility = View.INVISIBLE
+                check = 0
+            }else{
+                passwd.visibility = View.VISIBLE
+                check = 1
+            }
         }
     }
 }
